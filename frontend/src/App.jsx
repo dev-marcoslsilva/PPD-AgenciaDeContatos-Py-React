e633ff0627a825728bb39f28d42daba9e0cc5b61
Marcos Andre <marcosandre1309@gmail.com>
2026-04-20 23:17:58 -0300

    No porocesso de criação de tala principal. Erros anteriores resolvidos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,48 +29,64 @@ function App() {
   }, []);
 
   return (
-    <div className="flex items-center justify-around min-h-screen bg-gray-100 p-8">
+    <div className="flex-column items-center justify-around min-h-screen bg-gray-100 p-8">
+      <header className="bg-green-400 min-w-4xl min-h-16 flex items-center justify-center mb-8 rounded-lg shadow-md"></header>
 
-      <div className="max-w-2xl min-w-xl mx-auto bg-blue-400">
-        <h1 className="text-3xl text-white text-center">
-          Minha Agenda de Contatos
-        </h1>
+      <div className="flex items-center justify-center mt-4">
 
-        <div className="grid gap-4 py-4">
-          {contatos.map(contato => (
-            <div key={contato.id} className="bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-500">
-              <h2 className="text-xl font-semibold text-gray-700">{contato.nome}</h2>
-              <p className="text-gray-600">{contato.email}</p>
-              <p className="text-blue-600 font-medium">{contato.telefone}</p>
-            </div>
-          ))}
-          
-          {contatos.length === 0 && (
-            <p className="text-center text-gray-500">Nenhum contato encontrado.</p>
-          )}
+        <sidebar className="bg-blue-400 min-w-64 min-h-120 rounded-lg shadow-md p-4 mr-8">
+
+        </sidebar>
+        <div className="max-w-2xl min-w-100 max-h-100 mx-auto bg-blue-400">
+          <h1 className="text-3xl text-white text-center">
+            Minha Agenda de Contatos
+          </h1>
+
+          <div className="grid gap-4 py-4">
+            {contatos.map((contato) => (
+              <div
+                key={contato.id}
+                className="bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-500"
+              >
+                <h2 className="text-xl font-semibold text-gray-700">
+                  {contato.nome}
+                </h2>
+                <p className="text-gray-600">{contato.email}</p>
+                <p className="text-blue-600 font-medium">{contato.telefone}</p>
+              </div>
+            ))}
+
+            {contatos.length === 0 && (
+              <p className="text-center text-gray-500">
+                Nenhum contato encontrado.
+              </p>
+            )}
+          </div>
         </div>
-      </div>
 
-      <div className="max-w-2xl mx-auto bg-blue-400"> 
-        <h1>
-          Favoritos
-        </h1>
-          <div className='grid gap-8'>
-          {
-            favoritos.map((favoritos) => {
-              <div key={favoritos.id} className="bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-500">
-                <h2 className="text-xl font-semibold text-gray-700">{favoritos.nome}</h2>
+        <div className="max-w-2xl min-w-100 max-h-100 mx-auto bg-blue-400 rounded-xl">
+          <h1 className="text-3xl text-white text-center">Favoritos</h1>
+          <div className="grid gap-8">
+            {favoritos.map((favoritos) => (
+              <div
+                key={favoritos.id}
+                className="bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-500"
+              >
+                <h2 className="text-xl font-semibold text-gray-700">
+                  {favoritos.nome}
+                </h2>
                 <p>{favoritos.email}</p>
                 <p>{favoritos.telefone}</p>
               </div>
-              
-            })
-          }
+            ))}
 
-    {favoritos.length === 0 && (
-            <p className="text-center text-gray-500">Nenhum contato favoritado encontrado.</p>
-          )}
+            {favoritos.length === 0 && (
+              <p className="text-center text-gray-500">
+                Nenhum contato favoritado encontrado.
+              </p>
+            )}
           </div>
+        </div>
       </div>
     </div>
   );
